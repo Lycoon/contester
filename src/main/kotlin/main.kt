0f5ea8aa3e32d087951ae8a68b67af985ffe4318
erwan.vivien <erwan.vivien@epita.fr>
2021-05-11 23:07:50 +0200

    Can now sort by recent / popular

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,6 +4,7 @@ import twitter4j.TwitterFactory
 fun main() {
     val twitter = TwitterFactory.getSingleton()
     val query = Query("#contest")
+    query.resultType = Query.ResultType.recent
     val result = twitter.search(query)
 
     for (status in result.tweets)
