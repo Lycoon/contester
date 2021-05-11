193054ba32032a9b811ff9b5319412620b9869ed
hugo.bois <hugo.bois@epita.fr>
2021-05-11 22:07:04 +0200

    Added Twitter4J lib to Maven dependencies

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import twitter4j.Query
+import twitter4j.TwitterFactory
+
+fun main() {
+    val twitter = TwitterFactory.getSingleton()
+    val query = Query("#contest")
+    val result = twitter.search(query)
+
+    for (status in result.tweets)
+        println("@" + status.user.screenName.toString() + ":" + status.text)
 }
