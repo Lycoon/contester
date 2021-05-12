30dc877a6fc4ed51da984fd5e9d44f915fa01f07
erwan.vivien <erwan.vivien@epita.fr>
2021-05-12 17:32:11 +0200

    More kotlin way, replaced our loops by joinToString

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,30 +1,33 @@
 import twitter4j.Query
 import twitter4j.TwitterFactory
 
+
 fun main() {
     val twitter = TwitterFactory.getSingleton()
     val query = Query("(#contest OR #giveaway OR #concours OR #prize) +exclude:retweets +exclude:replies -airdrop")
     query.resultType(Query.ResultType.popular)
     query.count = 1
 
-    twitter.updateStatus("bruh")
-
-    for (i in 1..4) {
+    for (i in 1..1) {
         val result = twitter.search(query)
         for (status in result.tweets) {
             val text = status.text.toLowerCase()
             print("@" + status.user.screenName.toString() + ": ")
             print("" + text.contains("follow") + " ")
 
-            for (entity in status.hashtagEntities)
-                print(entity.text + " | ")
-            println()
-            print("\t")
+            println(
+                status.hashtagEntities.joinToString(
+                    separator = " | ",
+                    prefix = "\t[",
+                    postfix = "]"
+                ) { entity -> entity.text })
 
-            for (entity in status.userMentionEntities)
-                print(entity.text + " | ")
-
-            println()
+            println(
+                status.userMentionEntities.joinToString(
+                    separator = " | ",
+                    prefix = "\t[",
+                    postfix = "]"
+                ) { entity -> entity.text })
         }
 
         query.maxId = result.tweets.last().id - 1
